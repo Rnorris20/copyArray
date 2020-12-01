dab0c024bc2e8ceaa74a5a8e71c7e976f65c6398
RNorris6 <ryan.norris@patelhs.org>
2020-12-01 15:29:08 +0000

    created a print statement for the rosters.

--- Main.java
+++ Main.java
@@ -4,14 +4,13 @@ class Main {
     String[] roster1 = {"Ryan","George","James","Matthew","William","Marco","Lauren","Sam","Rob","Joe","James"};
     
 
-    int[] roster2 = new int[10];
+    String[] roster2 = new String[10];
   
    for (int i = 0; i <= roster1.length; i++)
      {
-          System.out.println(   roster1[i]   );
+          System.out.println(   "Roster2's new elements will be" + roster1[i]  );
      }
-  
+     
 
-    
   }
 }
