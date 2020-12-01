56aedbb1f61759e01af0344180f1dc03c56da915
RNorris6 <ryan.norris@patelhs.org>
2020-12-01 14:55:02 +0000

    created each individual roster

--- Main.java
+++ Main.java
@@ -1,10 +1,17 @@
 class Main {
   public static void main(String[] args) {
 
-    int[] roster1 = {}
+    String[] roster1 = {"Ryan","George","James","Matthew","William","Marco","Lauren","Sam","Rob","Joe","James"};
+    
 
-    int[] roster2 =
+    int[] roster2 = new int[10];
+  
+   for (int i = 0; i <= roster1.length; i++)
+     {
+          System.out.println(   roster1[i]   );
+     }
+  
 
-    System.out.println("Hello world!");
+    
   }
 }
