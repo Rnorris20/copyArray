925ae31756dd63603fc2aba5b1ad4a51c2d22923
RNorris6 <ryan.norris@patelhs.org>
2020-12-04 02:31:30 +0000

    fixed the for loop

--- Main.java
+++ Main.java
@@ -1,16 +1,28 @@
 class Main {
   public static void main(String[] args) {
 
+    String[] roster2 = new String[10];
+
     String[] roster1 = {"Ryan","George","James","Matthew","William","Marco","Lauren","Sam","Rob","Joe","James"};
     
-
-    String[] roster2 = new String[10];
+    
+    roster1[0] = 10;
+    roster1[1] = roster2[1];
+    System.out.println(roster1[0]);
+    System.out.println(roster1[1]);
   
    for (int i = 0; i <= roster1.length; i++)
-     {
-          System.out.println(   "Roster2's new elements will be" + roster1[i]  );
-     }
+     
+      for(int index = 2; index < roster1.length; index++){
+      //copying over roster2 into roster1
+      roster1[index] = roster2[index];
+      //print out roster1
+      System.out.println("roster1 =" + roster1[index]);
+      }
      
 
   }
 }
+
+int sum;
+    sum = 3;
